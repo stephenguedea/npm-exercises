703f07aa383c4285e1047a9ab94b4219aec2907e
Stephen Guedea <guedea.stephen@gmail.com>
2018-11-01 18:43:26 -0500

    Adding npm Exercises

---
+++ src/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// install jquery
+const $ = require('jquery');
+
+$('body').append('<h2>jQuery Successfully installed!</h2>');
+
+// function sayHello(input) {
+//     return input;
+// }
+//
+// console.log(sayHello('Hello'));
+
+
+
+// const sayHello = () => {
+//     console.log('Hey hey!');
+// }
+// sayHello();
