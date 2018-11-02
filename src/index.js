8a69d7e66f694bfb60e75b0c826619c1e699a75e
Stephen Guedea <guedea.stephen@gmail.com>
2018-11-02 10:56:46 -0500

    Adding Modules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 "use strict";
 
 // install jquery
-const $ = require('jquery');
-
-$('body').append('<h2>jQuery Successfully installed!</h2>');
+// const $ = require('jquery');
+//
+// $('body').append('<h2>jQuery Successfully installed!</h2>');
 
 // function sayHello(input) {
 //     return input;
@@ -17,3 +17,14 @@ $('body').append('<h2>jQuery Successfully installed!</h2>');
 //     console.log('Hey hey!');
 // }
 // sayHello();
+
+// MODULE EXERCISE
+
+// import user from './say-hello.js'
+// console.log(user.message);
+
+// import {morningGreet} from "./say-hello";
+// console.log(morningGreet);
+
+const { message } = require('./say-hello.js');
+console.log(message);
---
+++ src/say-hello.js
@@ -0,0 +1,10 @@
+// const message = 'Have a great day!';
+//
+// export default { message };
+
+// export const morningGreet = 'Good morning! You have a great day!';
+
+
+const message = 'You\'re the best, around!';
+
+module.exports = { message };
